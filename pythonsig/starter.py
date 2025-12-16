7bcb9da5553ad9a08c8488320526d64e996326b7
Vaishnavi Bendkule <vaishnavibendkule11@gmail.com>
2025-12-16 14:40:31 +0530

    First Upload

---
+++ pythonsig/starter.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage,SystemMessage
+
+load_dotenv()
+
+llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+messages=[
+    SystemMessage(content="you are a helpful assistant"),
+    HumanMessage(content="Write a poem abt AI in 4 lines")
+]
+
+result=llm.invoke(messages)
+print(result.content)
